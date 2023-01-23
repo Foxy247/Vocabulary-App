9138474330d6eb297018744bf86d2a08a9e2f7cc
Foxy247 <e.koelbl1@gmx.at>
2023-01-23 17:35:47 +0100

    asdf

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let dictionary = JSON.parse(localStorage.getItem('dictionary')) || {};
+let randomGermanWord;
 
 function addVocabulary(){
     dictionary[germanText.value] = englishText.value;
@@ -7,8 +8,28 @@ function addVocabulary(){
     englishText.value = '';
 
     localStorage.setItem ('dictionary', JSON.stringify(dictionary));
+    render();
 }
 
 function render() {
-    vocabularyList.innerHTML += `<li>Test</li> `
+    vocabularyList.innerHTML = '';
+    for (let key in dictionary){
+    vocabularyList.innerHTML += `<li>${key} - %{dictionary[key]} </li> `;
+    }
+}
+
+function nextVocabulary() {
+        let obj_keys = Object.keys(dictionary);
+        randomGermanWord = obj_keys[Math.floor(Math.random() *obj_keys.length)];
+        word.innerHTML = `${dictionary[randomGermanWord]}?`;
+}
+
+function check(){
+    if(germanText.value == randomGermanWord) {
+        text.innerHTML = 'Richtig!!!';
+    } else {
+        text.innerHTML = 'Falsch!!!';
+    }
+    germanText.value = '';
+    nextVocabulary();
 }
